621305ba7e7f2e75f71477a3fb9041d208e8386e
Aron Situ <arons@voicebox.com>
2013-03-06 17:39:02 -0800

    cool

---
+++ DiagonalMatrix.java
@@ -0,0 +1,43 @@
+public class DiagonalMatrix 
+{
+    public static void main(String[] args)
+    {
+        System.out.println("Hello World!");
+        int[][] Array = {
+                        {1, 2, 3},
+                        {4, 5, 6},
+                        {7, 8, 9}
+                        };
+        for(int i=0; i<Array.length; i++)
+        {
+            for(int j=0; j<Array[0].length; j++)
+            {
+                System.out.print("["+Array[i][j]+"]");
+            }
+            System.out.println("");
+        }
+        DiagonalMatrix(Array);
+    }
+    public static void DiagonalMatrix(int[][] matrix)
+    {
+        if(matrix != null)
+        {
+            int rowLen = matrix.length;
+            if(matrix[0] != null)
+            {
+                int colLen = matrix[0].length;
+                for(int i=0; i<rowLen; i++)
+                {
+                    for(int j=0; j<colLen; j++)
+                    {
+                        if(j-i >= 0)
+                            System.out.print("["+matrix[j][j-i]+"]");
+                        else
+                            System.out.print("[ ]");
+                    }
+                    System.out.println("");
+                }
+            }
+        }
+    }
+}
