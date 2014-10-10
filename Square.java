5e1ec92a1ea89b0ad7b41a67393a49c85f3cde03
bsdshell <000b000@gmail.com>
2014-10-09 21:33:21 -0700

    square root with margin error boundry

--- Square.java
+++ Square.java
@@ -3,14 +3,17 @@ class Square
 {
 	public static void main(String args[])
 	{
-		sqrt(10);
+        double n = 0.9;
+		sqrt(n);
+        double value = sqrt2(n, 0.00001);
+        System.out.println("real sqrt(value)=" + Math.sqrt(n));
+        System.out.println("value=" + value);
 	}
-	public static double sqrt(int n)
+	public static double sqrt(double n)
 	{
 		double a = 1.0;
 		double b = n/2;
 
-		
 		for(int i = 0; i < 10; i++)
 		{
 			if( ((a + b)/2)*((a + b)/2) > n)
@@ -29,4 +32,28 @@ class Square
 		}
 		return a;	
 	}
+    //Precondition: n is integer and greater than or equal zero 
+    public static double sqrt2(double n, double error)
+    {
+        double left = 0;
+        double right = n; 
+        double value = 0;
+        double diff = 2*error;
+        while(Math.abs(diff) > error)
+        {
+            value = (left + right)/2;
+            diff = value*value - n;
+            if(diff > 0)
+            {
+                right = value;
+            }
+            else if(diff < 0)
+            {
+                left = value;
+            }
+            else
+                break;
+        }
+        return value;
+    }
 }
