1832fc9dbedcaae560b497ef6c4e106e8a94751c
bsdshell <000b000@gmail.com>
2014-12-02 02:38:43 -0800

    Print Boundry Binary Tree

--- BinSearch2D.java
+++ BinSearch2D.java
@@ -4,20 +4,110 @@ public class BinSearch2D
 {
     public static void main(String[] args)
     {
-        System.out.println("binary search in two dimension");
-        
-        int[][] array = {{1, 10, 20, 21},
-                         {5, 15, 25, 100},
-                         {11,17, 30, 101},
-                         {12,18, 31, 102}};
-        int key = 25;
-        int rlo = 0;
-        int rhi = array.length-1;
-        int clo = 0;
-        int chi = array[0].length-1;
-        boolean ret = binSearch2D(array, rlo, rhi, clo, chi, key);
-        System.out.println("ret="+ret);
+        Test1();
     }
+
+    public static void printArray(int[][] array, int height, int width)
+    {
+        for(int col=0; col<=height; col++)
+        {
+            for(int row=0; row<=width; row++)
+            {
+                System.out.print("["+array[col][row]+"]");
+            }
+            System.out.println();
+        }
+    }
+    public static void Test1()
+    {
+        System.out.println("Binary search in two dimension");
+        int[][] array = {{1, 100, 200, 201},
+                         {5, 105, 205, 1000},
+                         {11,107, 300, 1001},
+                         {13,108, 302, 1002},
+                         {104,301, 305, 1003}};
+        int key = 5;
+        int rlo = 0;
+        int rhi = array[0].length-1;
+        int clo = 0;
+        int chi = array.length-1;
+        printArray(array, chi, rhi);
+
+        System.out.println("rhi=["+rhi+"]");
+        System.out.println("chi=["+chi+"]");
+
+        boolean ret1 = binSearch2DNew(array, clo, rlo, chi, rhi, key);
+        System.out.println("ret1="+ret1);
+
+    }
+    public static boolean binSearch2DNew(int[][] array, int collo, int rowlo, int colhi, int rowhi, int key)
+    {
+        int colmid = (collo + colhi)/2;
+        int rowmid = (rowlo + rowhi)/2;
+        boolean ret = false;
+        if(collo < colhi && rowlo < rowhi)
+        {
+            if(key < array[colmid][rowmid])
+            {
+                boolean b2 = binSearch2DNew(array, collo, rowlo, colmid-1, rowmid-1, key);
+                boolean bup = binSearch2DNew(array, collo, rowmid, colmid-1, rowhi, key);
+                boolean bdown = binSearch2DNew(array, colmid, rowlo, colhi, rowmid-1, key);
+                ret = b2 || bup || bdown;
+            }
+            else if(key > array[colmid][rowmid])
+            {
+                boolean b3 = binSearch2DNew(array, colmid+1, rowmid+1, colhi, rowhi, key);
+                boolean bup = binSearch2DNew(array, collo, rowmid, colmid, rowhi, key);
+                boolean bdown = binSearch2DNew(array, colmid+1, rowlo, colhi, rowmid, key);
+                ret = b3 || bup || bdown;
+            }
+            else
+            {
+                System.out.println("["+colmid+"]["+rowmid+"]key["+key+"]");
+                ret = true;
+            }
+        }
+        else if(collo == colhi && rowlo < rowhi)
+        {
+            if(key < array[colmid][rowmid])
+            {
+                ret = binSearch2DNew(array, colmid, rowlo, colmid, rowmid-1, key);
+            }
+            else if(key > array[colmid][rowmid])
+            {
+                ret = binSearch2DNew(array, colmid, rowmid+1, colmid, rowhi, key);
+            }
+            else
+            {
+                System.out.println("["+colmid+"]["+rowmid+"]key["+key+"]");
+                ret = true;
+            }
+        }
+        else if(collo < colhi && rowlo == rowhi)
+        {
+            if(key < array[colmid][rowmid])
+            {
+                ret = binSearch2DNew(array, collo, rowmid, colmid-1, rowmid, key);
+            }
+            else if(key > array[colmid][rowmid])
+            {
+                ret = binSearch2DNew(array, colmid+1, rowmid, colhi, rowmid, key);
+            }
+            else
+            {
+                System.out.println("["+colmid+"]["+rowmid+"]key["+key+"]");
+                ret = true;
+            }
+        }
+        else if(collo == colhi && rowlo == rowhi)
+        {
+            ret = key == array[colmid][rowmid];
+            if(ret)
+                System.out.println("["+colmid+"]["+rowmid+"]key["+key+"]");
+        }
+        return ret;
+    }
+
     public static boolean binSearch2D(int[][] array, int rlo, int rhi, int clo, int chi, int key)
     {
         boolean ret = false;
---
+++ BoundryBinaryTree.java
@@ -0,0 +1,239 @@
+import java.io.*;
+import java.lang.String;
+import java.util.*;
+
+class Node
+{
+	public Node left;
+	public Node right;
+	public int data;
+	boolean visited;
+	public Node(int n)
+	{ data = n; left = null; right = null; visited=false;}
+	public Node getLeft()
+	{ return left; }
+	public Node getRight()
+	{ return right; }
+	public void setLeft(Node l)
+	{ left = l;}
+	public void setRight(Node r)
+	{ right = r;}
+}
+
+class BST
+{
+	Node root;
+	public BST()
+	{ root = null;};
+	public void Insert(int n)
+	{
+		if(root == null)
+		{ root = new Node(n); }
+		else
+		{ 
+			Node cur = root;
+			boolean end = false;
+			while(cur != null && !end)
+			{
+				if(n < cur.data)
+				{ 
+					if(cur.left == null)
+					{ cur.left = new Node(n); end = true; }
+					else 
+						cur = cur.left;
+				}
+				else 
+				{
+					if(cur.right == null)
+					{ cur.right = new Node(n); end = true; }
+					else
+						cur = cur.right;
+				}
+			}
+		}
+	}
+	public void Inorder(Node cur)
+	{
+		if(cur != null)
+		{
+			Inorder(cur.left);
+			System.out.print(cur.data + " ");
+			Inorder(cur.right);
+		}
+	}
+	public Node getRoot()
+	{ return root;}
+}
+class BoundryBinaryTree 
+{
+
+	public static Node prev = null;
+	public static void main(String args[])
+	{
+		BST b1 = new BST();
+		b1.Insert(50);
+        
+		b1.Insert(39);
+		b1.Insert(18);
+		b1.Insert(35);
+		b1.Insert(36);
+		b1.Insert(41);
+		b1.Insert(43);
+        
+        b1.Insert(51);
+		b1.Insert(91);
+		b1.Insert(76);
+		b1.Insert(60);
+		b1.Insert(99);
+
+		
+        
+        System.out.println();
+        PrintBoundry(b1.getRoot());
+        System.out.println();
+        Node right = null;
+        //InorderLeftLeafNode(b1.getRoot(), right); 
+        System.out.println();
+        PrintBoundryWithInorder(b1.getRoot());
+	}
+	
+    //Initialize right = null
+    static boolean leaf = false;
+    public static void InorderLeftLeafNode(Node curr)
+    {
+        if(curr != null)
+        {
+            if(!leaf)
+                System.out.print("{" + curr.data + "}");
+            if(curr.left == null && curr.right == null)
+            {
+                if(leaf)
+                    System.out.print("[" + curr.data + "]");
+                leaf = true;
+            }
+            InorderLeftLeafNode(curr.left);
+            InorderLeftLeafNode(curr.right);
+        }
+    }
+    public static void PrintBoundryWithInorder(Node curr)
+    {
+        InorderLeftLeafNode(curr);
+        if(curr != null)
+        {
+            Node right = curr.right;
+            Stack<Node> stack = new Stack<Node>();
+            while(right != null)
+            {
+                if(right.left != null || right.right != null)
+                {
+                    stack.push(right);
+                    //System.out.print("[" + right.data + "]");
+                }
+                right = right.right;
+            }
+            while(!stack.empty())
+            {
+                System.out.print("[" + stack.pop().data + "]");
+            }
+        }
+            
+
+    }
+    public static void PrintBoundry(Node curr)
+    {
+        Stack<Node> st = new Stack<Node>();
+        Set<Integer> hashset = new HashSet<Integer>();
+        boolean first = false;
+        Node root = curr;
+        while(curr != null || !st.empty())
+        {
+            if(curr != null)
+            {
+                if(!first)
+                {
+                    System.out.println("[" + curr.data + "]");
+                    hashset.add(curr.data);
+                }
+                st.push(curr);
+                curr = curr.left;
+            }
+            else
+            {
+                if(!first)
+                    first = true;
+                Node node = st.pop();
+                if(node.left == null && node.right == null)
+                {
+                    if(!hashset.contains(node.data))
+                    {
+                        System.out.println("[" + node.data + "]");
+                        hashset.add(node.data);
+                    }
+                }
+                curr = node.right;
+            }
+        }
+        //Print out the nodes in the right side of the tree
+        if(root != null)
+        {
+            Stack<Node> stack = new Stack<Node>();
+            Node right = root.right;
+            while(right != null)
+            {
+                if(!hashset.contains(right.data))
+                {
+                    hashset.add(right.data);
+                    stack.push(right);
+                }
+                right = right.right;
+            }
+            while(!stack.empty())
+            {
+                System.out.println("[" + stack.pop().data + "]");
+            }
+        }
+    }
+
+    public static void PostOrder(Node r)
+    {
+        if( r != null)
+        {
+            PostOrder(r.left);
+            PostOrder(r.right);
+            System.out.println("normal[" + r.data + "]");
+        }
+    }
+
+    public static void PostorderSimple(Node root)
+    {
+        if(root != null)
+        {
+            Stack<List<Node>> st = new Stack<List<Node>>();
+            
+            while(root != null || !st.empty())
+            {
+                if(root != null)
+                {
+                    List<Node> list = new LinkedList<Node>();
+                    list.add(0, root);
+                    if(root.left != null)
+                        list.add(1, root.left);
+                    if(root.right != null)
+                        list.add(2, root.right);
+
+                    root = root.left;
+                }
+            }
+        }
+    }
+        
+	public static void Inorder(Node root)
+	{
+		if(root != null)
+		{
+			Inorder(root.left);
+			System.out.println("data=" + root.data);
+			Inorder(root.right);
+		}
+	}
+}
