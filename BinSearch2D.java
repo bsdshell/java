334e767113ba790884b0a4468759a7a14b3eeb80
bsdshell <000b000@gmail.com>
2014-12-24 18:21:11 -0800

    Update 2D binary search

--- BinSearch2D.java
+++ BinSearch2D.java
@@ -21,12 +21,12 @@ public class BinSearch2D
     public static void Test1()
     {
         System.out.println("Binary search in two dimension");
-        int[][] array = {{1, 100, 200, 201},
-                         {5, 105, 205, 1000},
-                         {11,107, 300, 1001},
-                         {13,108, 302, 1002},
+        int[][] array = {{1,  100, 200, 201},
+                         {5,  105, 205, 1000},
+                         {11, 107, 300, 1001},
+                         {13, 108, 302, 1002},
                          {104,301, 305, 1003}};
-        int key = 5;
+        int key = 201;
         int rlo = 0;
         int rhi = array[0].length-1;
         int clo = 0;
@@ -39,6 +39,34 @@ public class BinSearch2D
         boolean ret1 = binSearch2DNew(array, clo, rlo, chi, rhi, key);
         System.out.println("ret1="+ret1);
 
+        boolean ret2 = binSearch2DBetter(array, clo, rlo, chi, rhi, key);
+        System.out.println("ret2="+ret2);
+
+        boolean ret3 = binSearchMN(array, clo, rlo, chi, rhi, key);
+        System.out.println("ret3="+ret3);
+    }
+    public static boolean binSearchMN(int[][] array, int collo, int rowlo, int colhi, int rowhi, int key)
+    {
+        int width = rowhi - rowlo + 1;
+        int height = colhi - collo + 1;
+        int col = collo;
+        int row = rowhi;
+        for(int i=0; i<width+height; i++)
+        {
+            if(key < array[col][row])
+            {
+                if(row > rowlo)
+                    row--;
+            }
+            else if(key > array[col][row])
+            {
+                if(col < colhi)
+                    col++;
+            }
+            else 
+                return true;
+        }
+        return false;
     }
     public static boolean binSearch2DNew(int[][] array, int collo, int rowlo, int colhi, int rowhi, int key)
     {
@@ -107,6 +135,44 @@ public class BinSearch2D
         }
         return ret;
     }
+    public static boolean binSearch2DBetter(int[][] array, int collo, int rowlo, 
+                                                           int colhi, int rowhi, int key)
+    {
+        if(array != null)
+        {
+            int colm = (collo + colhi)/2;
+            int rowm = (rowlo + rowhi)/2;
+            if(collo <= colhi && rowlo <= rowhi)
+            {
+                if(key < array[colm][rowm])
+                {
+                    boolean b1 = false, b2 = false, b3 = false;
+                    b1 = binSearch2DBetter(array, collo, rowlo,
+                                                  colm-1, rowm-1, key);
+
+                    b2 = binSearch2DBetter(array, collo, rowm,
+                                                  colm-1,rowhi, key);
+                    b3 = binSearch2DBetter(array, colm, rowlo,
+                                                  colhi, rowm-1, key);
+                    return b1 || b2 || b3;
+                }
+                else if(key > array[colm][rowm])
+                {
+                    boolean b1 = false, b2 = false, b3 = false;
+                    b1 = binSearch2DBetter(array, colm+1, rowm+1,
+                                                  colhi,  rowhi, key);
+                    b2 = binSearch2DBetter(array, collo, rowm+1,
+                                                  colm,  rowhi, key);
+                    b3 = binSearch2DBetter(array, colm+1, rowlo,
+                                                  colhi,  rowm, key);
+                    return b1 || b2 || b3;
+                }
+                else
+                    return true;
+            }
+        }
+        return false;
+    } 
 
     public static boolean binSearch2D(int[][] array, int rlo, int rhi, int clo, int chi, int key)
     {
