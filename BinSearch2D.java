56555296c23f86a6256df0d1adef57086130a882
bsdshell <000b000@gmail.com>
2014-12-25 21:07:41 -0800

    Update two dimension search algorithm

--- BinSearch2D.java
+++ BinSearch2D.java
@@ -33,19 +33,17 @@ public class BinSearch2D
         int chi = array.length-1;
         printArray(array, chi, rhi);
 
-        System.out.println("rhi=["+rhi+"]");
-        System.out.println("chi=["+chi+"]");
-
-        boolean ret1 = binSearch2DNew(array, clo, rlo, chi, rhi, key);
-        System.out.println("ret1="+ret1);
-
-        boolean ret2 = binSearch2DBetter(array, clo, rlo, chi, rhi, key);
+        boolean ret2 = BinSearch2D(array, clo, rlo, chi, rhi, key);
         System.out.println("ret2="+ret2);
 
         boolean ret3 = binSearchMN(array, clo, rlo, chi, rhi, key);
         System.out.println("ret3="+ret3);
     }
-    public static boolean binSearchMN(int[][] array, int collo, int rowlo, int colhi, int rowhi, int key)
+
+    //Binary search from top right corder, eliminate row/column algorithm 
+    //Complexity O(m+n)
+    public static boolean binSearchMN(int[][] array, int collo, int rowlo, 
+                                                     int colhi, int rowhi, int key)
     {
         int width = rowhi - rowlo + 1;
         int height = colhi - collo + 1;
@@ -68,75 +66,11 @@ public class BinSearch2D
         }
         return false;
     }
-    public static boolean binSearch2DNew(int[][] array, int collo, int rowlo, int colhi, int rowhi, int key)
-    {
-        int colmid = (collo + colhi)/2;
-        int rowmid = (rowlo + rowhi)/2;
-        boolean ret = false;
-        if(collo < colhi && rowlo < rowhi)
-        {
-            if(key < array[colmid][rowmid])
-            {
-                boolean b2 = binSearch2DNew(array, collo, rowlo, colmid-1, rowmid-1, key);
-                boolean bup = binSearch2DNew(array, collo, rowmid, colmid-1, rowhi, key);
-                boolean bdown = binSearch2DNew(array, colmid, rowlo, colhi, rowmid-1, key);
-                ret = b2 || bup || bdown;
-            }
-            else if(key > array[colmid][rowmid])
-            {
-                boolean b3 = binSearch2DNew(array, colmid+1, rowmid+1, colhi, rowhi, key);
-                boolean bup = binSearch2DNew(array, collo, rowmid, colmid, rowhi, key);
-                boolean bdown = binSearch2DNew(array, colmid+1, rowlo, colhi, rowmid, key);
-                ret = b3 || bup || bdown;
-            }
-            else
-            {
-                System.out.println("["+colmid+"]["+rowmid+"]key["+key+"]");
-                ret = true;
-            }
-        }
-        else if(collo == colhi && rowlo < rowhi)
-        {
-            if(key < array[colmid][rowmid])
-            {
-                ret = binSearch2DNew(array, colmid, rowlo, colmid, rowmid-1, key);
-            }
-            else if(key > array[colmid][rowmid])
-            {
-                ret = binSearch2DNew(array, colmid, rowmid+1, colmid, rowhi, key);
-            }
-            else
-            {
-                System.out.println("["+colmid+"]["+rowmid+"]key["+key+"]");
-                ret = true;
-            }
-        }
-        else if(collo < colhi && rowlo == rowhi)
-        {
-            if(key < array[colmid][rowmid])
-            {
-                ret = binSearch2DNew(array, collo, rowmid, colmid-1, rowmid, key);
-            }
-            else if(key > array[colmid][rowmid])
-            {
-                ret = binSearch2DNew(array, colmid+1, rowmid, colhi, rowmid, key);
-            }
-            else
-            {
-                System.out.println("["+colmid+"]["+rowmid+"]key["+key+"]");
-                ret = true;
-            }
-        }
-        else if(collo == colhi && rowlo == rowhi)
-        {
-            ret = key == array[colmid][rowmid];
-            if(ret)
-                System.out.println("["+colmid+"]["+rowmid+"]key["+key+"]");
-        }
-        return ret;
-    }
-    public static boolean binSearch2DBetter(int[][] array, int collo, int rowlo, 
-                                                           int colhi, int rowhi, int key)
+    
+    //Binary search: eliminate one quandrant in each step
+    //Complexity O(lg(m)+lg(n))
+    public static boolean BinSearch2D(int[][] array, int collo, int rowlo, 
+                                                     int colhi, int rowhi, int key)
     {
         if(array != null)
         {
@@ -147,23 +81,23 @@ public class BinSearch2D
                 if(key < array[colm][rowm])
                 {
                     boolean b1 = false, b2 = false, b3 = false;
-                    b1 = binSearch2DBetter(array, collo, rowlo,
+                    b1 = BinSearch2D(array, collo, rowlo,
                                                   colm-1, rowm-1, key);
 
-                    b2 = binSearch2DBetter(array, collo, rowm,
+                    b2 = BinSearch2D(array, collo, rowm,
                                                   colm-1,rowhi, key);
-                    b3 = binSearch2DBetter(array, colm, rowlo,
+                    b3 = BinSearch2D(array, colm, rowlo,
                                                   colhi, rowm-1, key);
                     return b1 || b2 || b3;
                 }
                 else if(key > array[colm][rowm])
                 {
                     boolean b1 = false, b2 = false, b3 = false;
-                    b1 = binSearch2DBetter(array, colm+1, rowm+1,
+                    b1 = BinSearch2D(array, colm+1, rowm+1,
                                                   colhi,  rowhi, key);
-                    b2 = binSearch2DBetter(array, collo, rowm+1,
+                    b2 = BinSearch2D(array, collo, rowm+1,
                                                   colm,  rowhi, key);
-                    b3 = binSearch2DBetter(array, colm+1, rowlo,
+                    b3 = BinSearch2D(array, colm+1, rowlo,
                                                   colhi,  rowm, key);
                     return b1 || b2 || b3;
                 }
@@ -173,67 +107,4 @@ public class BinSearch2D
         }
         return false;
     } 
-
-    public static boolean binSearch2D(int[][] array, int rlo, int rhi, int clo, int chi, int key)
-    {
-        boolean ret = false;
-        if( array != null && array.length > 0 && array[0].length > 0)
-        {
-            int rmid = (rlo + rhi)/2;
-            int cmid = (clo + chi)/2;
-            if(rhi > rlo && chi > clo)
-            {
-                if(key < array[rmid][cmid])
-                {
-                    boolean ret1 = binSearch2D(array, rlo, rmid, clo, chi, key);
-                    boolean ret2 = binSearch2D(array, rmid+1, rhi, clo, cmid, key);
-                    ret = ret1 || ret2;
-                }
-                else if(key > array[rmid][cmid])
-                {
-                    boolean ret1 = binSearch2D(array, rlo, rhi, cmid+1, chi, key);
-                    boolean ret2 = binSearch2D(array, rmid+1, rhi, clo, cmid+1, key);
-                    ret = ret1 || ret2;
-                }
-                else
-                    ret = true;
-            }
-            else if(rhi > rlo && chi == clo)
-            {
-                if(key < array[rmid][cmid])
-                {
-                    ret = binSearch2D(array, rlo, rmid, cmid, cmid, key);
-                }
-                else if(key > array[rmid][cmid])
-                {
-                    ret = binSearch2D(array, rmid+1, rhi, cmid, cmid, key);
-                }
-                else
-                {
-                    ret = true;
-                }
-            }
-            else if(rhi == rlo && chi > clo)
-            {
-                if(key < array[rmid][cmid])
-                {
-                    ret = binSearch2D(array, rmid, rmid, clo, cmid, key);
-                }
-                else if(key > array[rmid][cmid])
-                {
-                    ret = binSearch2D(array, rmid, rmid, cmid+1, chi, key);
-                }
-                else
-                {
-                    ret = true;
-                }
-            }
-            else
-            {
-                if(key == array[rmid][cmid])
-                    ret = true;
-            }
-        }
-        return ret;
-    }
 }
