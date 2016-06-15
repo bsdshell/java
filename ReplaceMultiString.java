225a4ef70f610816c7cea47a2b7bb2c564a006a6
bsdshell <monosurface@gmail.com>
2016-06-14 18:07:19 -0700

    anagrams

--- ReplaceMultiString.java
+++ ReplaceMultiString.java
@@ -8,26 +8,25 @@ import java.util.Map;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
-public class ReplaceMultiString 
-{
-    public static void main( String args[] )
-    {
-        Map<String, String> replacements = new HashMap<String, String>()
-        {{
-            put(":", "colon");
-            put(",", "common");
-            put(" '", "[ ]");
-        }};
-        
-        String input = " ' cool :";
+public class ReplaceMultiString {
+    public static void main( String args[] ) {
+        // java regular, java regex, jregex
+        Map<String, String> replacements = new HashMap<String, String>() {
+            {
+                put(":", "colon");
+                put("#", "number_sign");
+                put("$", "dollar");
+            }
+        };
 
-        String regexp = "' ,'|:| '";
+        String input = "$dog";
+
+        String regexp = "\\$";
         StringBuffer sb = new StringBuffer();
         Pattern pattern = Pattern.compile(regexp);
         Matcher match = pattern.matcher(input);
 
-        while(match.find())
-        {
+        while(match.find()) {
             match.appendReplacement(sb, replacements.get(match.group()));
         }
         match.appendTail(sb);
