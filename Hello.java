0536ae5526169ac5ceb428962bc570151ca4de56
bsdshell <monosurface@gmail.com>
2016-07-03 20:07:26 -0700

    update

--- Hello.java
+++ Hello.java
@@ -8,66 +8,110 @@ import classfile.*;
 
 public class Hello {
     public static void main(String[] args) {
-        System.out.println("---------------------------------\n");
         test0();
-        test1();
         test2();
+        test3();
+        test4();
+        test5();
+        test6();
     }
-    static void test0() {
-        System.out.println("---------------------------------\n");
-        BST bst = new BST();
-        bst.insert(10);
-        bst.insert(5);
-        bst.insert(15);
-        System.out.println("\n---------------------------------");
-        Aron.postorder(bst.root);
-        System.out.println("\n---------------------------------");
-        postIteration(bst.root);
-        System.out.println("---------------------------------\n");
-    }
-    static void test1() {
-        System.out.println("---------------------------------\n");
-        BST bst = new BST();
-        bst.insert(10);
-        System.out.println("\n---------------------------------");
-        Aron.postorder(bst.root);
-        System.out.println("\n---------------------------------");
-        postIteration(bst.root);
-        System.out.println("---------------------------------\n");
-    }
-    static void test2() {
-        System.out.println("---------------------------------\n");
-        BST bst = new BST();
-        bst.insert(10);
-        bst.insert(5);
-        bst.insert(15);
-        bst.insert(8);
-        System.out.println("\n---------------------------------");
-        Aron.postorder(bst.root);
-        System.out.println("\n---------------------------------");
-        postIteration(bst.root);
-        System.out.println("---------------------------------\n");
+    
+    static void test0(){
+        System.out.println("\n--------------------------------------------------------------------------------");
+        Aron.name();
+        SingleLinkedList sll = new SingleLinkedList();
+        sll.append(5);
+        sll.append(4);
+        sll.append(2);
+        Aron.printSLL(sll.head);
+        SNode newHead = cloneLinkedList(sll.head); 
+        Aron.printSLL(newHead);
+        System.out.println("\n--------------------------------------------------------------------------------");
     }
 
-    public static void postIteration(Node root) {
-        System.out.println("\n---------------------------------");
-        Stack<Node> stack = new Stack<Node>();
-        Node curr = root;
-        while(curr != null || stack.isEmpty() == false) {
-            if(curr != null) {
-                stack.push(curr);
-                curr = curr.left;
-            } else {
-                Node node = stack.peek();
-                if(node.isVisited) {
-                    System.out.println("[" + node.data + "]");
-                    stack.pop();
-                } else {
-                    node.isVisited = true;
-                    curr = node.right;
-                }
+    static void test2(){
+        System.out.println("\n--------------------------------------------------------------------------------");
+        Aron.name();
+        SingleLinkedList sll = new SingleLinkedList();
+        sll.append(1);
+        sll.append(2);
+        sll.append(3);
+        Aron.printSLL(sll.head);
+        SNode newHead = reversePair(sll.head); 
+        Aron.printSLL(newHead);
+        System.out.println("\n--------------------------------------------------------------------------------");
+    }
+
+    static void test3(){
+        System.out.println("\n--------------------------------------------------------------------------------");
+        Aron.name();
+        SingleLinkedList sll = new SingleLinkedList();
+        sll.append(1);
+        sll.append(2);
+        sll.append(3);
+        sll.append(4);
+        Aron.printSLL(sll.head);
+        SNode newHead = reversePair(sll.head); 
+        Aron.printSLL(newHead);
+        System.out.println("\n--------------------------------------------------------------------------------");
+    }
+    static void test4(){
+        System.out.println("\n--------------------------------------------------------------------------------");
+        Aron.name();
+        SingleLinkedList sll = new SingleLinkedList();
+        sll.append(1);
+        sll.append(2);
+        Aron.printSLL(sll.head);
+        SNode newHead = reversePair(sll.head); 
+        Aron.printSLL(newHead);
+        System.out.println("\n--------------------------------------------------------------------------------");
+    }
+    static void test5(){
+        System.out.println("\n--------------------------------------------------------------------------------");
+        Aron.name();
+        SingleLinkedList sll = new SingleLinkedList();
+        sll.append(1);
+        Aron.printSLL(sll.head);
+        SNode newHead = reversePair(sll.head); 
+        Aron.printSLL(newHead);
+        System.out.println("\n--------------------------------------------------------------------------------");
+    }
+    static void test6(){
+        System.out.println("\n--------------------------------------------------------------------------------");
+        Aron.name();
+        SingleLinkedList sll = new SingleLinkedList();
+        Aron.printSLL(sll.head);
+        SNode newHead = reversePair(sll.head); 
+        Aron.printSLL(newHead);
+        System.out.println("\n--------------------------------------------------------------------------------");
+    }
+
+    public static SNode reversePair(SNode head){
+        SNode curr = head;
+        while(curr != null && curr.next != null){
+            int tmp = curr.data;
+            curr.data = curr.next.data;
+            curr.next.data = tmp;
+
+            curr = curr.next.next;
+        }
+        return head;
+    }
+
+    static SNode cloneLinkedList(SNode head) {
+        SNode newHead = null;
+        SNode newCurr = null;
+        SNode curr = head;
+
+        if(curr != null) {
+            newCurr = newHead = new SNode(head.data);
+
+            while(curr.next != null) {
+                newCurr.next = new SNode(curr.next.data);
+                newCurr = newCurr.next;
+                curr = curr.next;
             }
         }
-        System.out.println("\n---------------------------------");
+        return newHead;
     }
 }
