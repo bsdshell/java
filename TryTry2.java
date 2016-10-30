bf2e33d5164788523f3848f89bbc413af4113991
bsdshell <monosurface@gmail.com>
2016-10-29 19:23:26 -0700

    Add Spawn Process, complete process

--- TryTry2.java
+++ TryTry2.java
@@ -3,11 +3,9 @@ import java.io.*;
 import classfile.*;
 import org.apache.commons.lang3.StringUtils;
 
-// padding example 
-// use apache common lib
-// jar file /Users/cat/myfile/github/JavaLib/commons-lang3-3.5.jar
-public class PaddingExample{
+public class TryTry2 {
     public static void main(String[] args) {
+        Print.pbl("do");
         String s1 = StringUtils.leftPad("", 3, 'z');
         Print.pbl(s1);
         String s2 = StringUtils.leftPad("bat", 5, 'z');
--- graphexample.java
+++ graphexample.java
@@ -2,27 +2,68 @@ import java.util.*;
 import java.io.*;
 import classfile.*;
 
-public class graphexample{
+// leftPad()
+import org.apache.commons.lang3.StringUtils;
+
+public class GraphExample{
     public static void main(String[] args) {
         test0();
         test1();
+        test2();
     }
     public static void test0(){
         Aron.beg();
+        int level = 0;
         Node root = createGraph();
         preorderGraph(root);
+        preorderGraph(root, level);
         Aron.line();
         postorderGraph(root);
         Aron.end();
     }
     public static void test1(){
         Aron.beg();
+        BST b1 = new BST();
+        b1.insert(10);
+        b1.insert(7);
+        b1.insert(15);
+        b1.insert(1);
+        b1.insert(5); 
+        b1.insert(0); 
+
+        b1.print();
+        Aron.line();
+        int level = 0;
+        Aron.prettyPrint(b1.root, level);
+
         Aron.end();
     }
+    static void test2(){
+        Aron.beg();
+        BST b1 = new BST();
+        b1.insert(10);
+        b1.insert(5);
+        b1.insert(16);
+        b1.insert(115);
+        b1.insert(135);
+        b1.insert(15);
+        b1.insert(1);
+        b1.insert(1145);
+        b1.insert(7); 
+
+        int level = 0;
+        b1.print();
+        Aron.line();
+        Aron.prettyPrint(b1.root, level);
+        Aron.binImage(b1.root);
+        
+        Aron.end();
+    } 
+
     public static Node createGraph(){
         Aron.beg();
         Node root = new Node(1);
-        Node n1= new Node(2);
+        Node n1 = new Node(2);
         Node n2 = new Node(3);
         Node n3 = new Node(4);
 
@@ -30,7 +71,7 @@ public class graphexample{
         root.list.add(n2);
         root.list.add(n3);
 
-        Node nn1= new Node(22);
+        Node nn1 = new Node(22);
         Node nn2 = new Node(33);
         Node nn3 = new Node(44);
 
@@ -54,6 +95,16 @@ public class graphexample{
             Print.pbl(curr.data);
             for(Node n : curr.list){
                 preorderGraph(n);
+            }
+        }
+    }
+
+    public static void preorderGraph(Node curr, int level){
+        if(curr != null){
+            String s = StringUtils.leftPad("", 2*level, '-');
+            Print.pl(s + "[" + curr.data + "]");
+            for(Node n : curr.list){
+                preorderGraph(n, level + 1);
             }
         }
     }
