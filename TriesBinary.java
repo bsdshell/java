4683b3d17bfbd8ffe2467889570b5db81776005c
bsdshell <monosurface@gmail.com>
2017-01-24 17:55:07 -0800

    Binary Tries

--- TriesBinary.java
+++ TriesBinary.java
@@ -9,20 +9,43 @@ import java.util.stream.*;
 // count the maximum different bits in binary matrix
 class TNode{
     public TNode[] arr = new TNode[2];
+    public boolean isWord = false;
+    public TNode(boolean isWord){
+        this.isWord = isWord;
+    }
+    public TNode(){
+    }
 }
 
 public class TriesBinary{
     public static void main(String[] args) {
-        test0();
-        test1();
+//        test0();
+//        test1();
+        test2();
     }
     public static void insert(TNode node, String s, int index){
         if(index < s.length()){
             int inx = s.charAt(index) - '0';
             if(node.arr[inx] == null)
-                node.arr[inx] = new TNode();
+                node.arr[inx] = new TNode(false);
             insert(node.arr[inx], s, index+1);
+        }else{
+            node.isWord = true;
         }
+    }
+    public static boolean contains(TNode root, String s, int index){
+        if(s != null){
+            if(index < s.length()){
+                int n = s.charAt(index) - '0';    
+                if(root.arr[n] == null)
+                    return false;
+                else
+                    return contains(root.arr[n], s, index+1);
+            }else{
+                return root.isWord;
+            }
+        }
+        return false;
     }
     
     public static void print(TNode node){
@@ -56,7 +79,7 @@ public class TriesBinary{
         String s1 = "011";
         String s2 = "111";
         String s3 = "010";
-        TNode root = new TNode();
+        TNode root = new TNode(true);
         int index = 0;
         insert(root, s1, index);
         insert(root, s2, index);
@@ -64,6 +87,7 @@ public class TriesBinary{
         print(root);
         Aron.end();
     }
+
     public static void test1(){
         Aron.beg();
         String[] array = {
@@ -78,9 +102,8 @@ public class TriesBinary{
         TNode root = new TNode();
         int index = 0;
 
-        insert(root, arr[0], index);
         for(int i=1; i<array.length; i++){
-            (root, str, index);
+            insert(root, array[i], index);
         }
 
         int num = countDiff(root);
@@ -88,5 +111,27 @@ public class TriesBinary{
 
         Aron.end();
     }
+    static void test2(){
+        Aron.beg();
+
+        String[] array = {
+            "011",
+            "111",
+            "010",
+        };
+        TNode root = new TNode();
+        int index = 0;
+
+        for(int i=1; i<array.length; i++){
+            insert(root, array[i], index);
+        }
+        
+        boolean b = contains(root, "111", index);
+        Test.t(b == true);
+        b = contains(root, "000", index);
+        Test.t(b == false);
+
+        Aron.end();
+    } 
 } 
 
