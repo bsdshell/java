664ee888f1d43f519b0bb25afcb5c1e628b4fcea
bsdshell <monosurface@gmail.com>
2017-03-14 00:49:29 -0700

    gradle add libs

---
+++ GradleAddLib/src/test/java/MyClassTest.java
@@ -0,0 +1,35 @@
+
+import classfile.Print;
+import org.junit.After;
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * Created by cat on 3/13/17.
+ */
+public class MyClassTest {
+    @Before
+    public void setUp() throws Exception {
+
+    }
+
+    @After
+    public void tearDown() throws Exception {
+
+    }
+
+    @Test
+    public void myClass() throws Exception {
+
+    }
+
+    @Test
+    public void print() throws Exception {
+        MyClass c = new MyClass();
+        assertTrue(c.print() == 1);
+        Print.pbl("test file");
+    }
+
+}
---
+++ GradleFirst/src/main/java/MyClass.java
@@ -0,0 +1,8 @@
+
+public class MyClass {
+    public void MyClass(){
+    }
+    public int print(){
+        return 1;
+    }
+}
