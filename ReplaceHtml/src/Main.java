e41d0e6651ae6078238d1a1344537f6116344c96
bsdshell <monosurface@gmail.com>
2017-03-30 01:06:25 -0700

    simple encryption and decryption

--- ReplaceHtml/src/Main.java
+++ ReplaceHtml/src/Main.java
@@ -1,17 +1,31 @@
 import classfile.Print;
 import classfile.Aron;
+import classfile.Test;
+import classfile.Ut;
 import org.apache.commons.io.FilenameUtils;
 
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.List;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+final class XAron{
+    public static FilenameUtils filenameUtils;
+}
+
+
 
 public class Main {
     public static void main(String[] args) {
         Print.p("Hello World");
-        test0();
-        test1();
+//        test0();
+//        test1();
+//        test2();
+
+        test3();
     }
 
     static void test0() {
@@ -61,11 +75,12 @@ public class Main {
             middleList.add(0, "<!-- Column 2 start -->\n          <ul>" + "\n");
             middleList.add("</ul>\n<!-- Column 2 end -->" + "\n");
 
-            List<String> bigList = Aron.mergeLists(topList, Aron.mergeLists(middleList, bottomList));
+            List<String> bigList = Aron.mergeListsDuplicate(topList, Aron.mergeListsDuplicate(middleList, bottomList));
             Aron.printListLn(bigList);
             Aron.line();
 
             String newName = FilenameUtils.getFullPath(name) + "new/" + FilenameUtils.getName(name);
+
             Print.pbl("newName" + newName);
             Aron.writeFile(newName, bigList);
 
@@ -74,8 +89,82 @@ public class Main {
     }
     static void test1(){
         Aron.beg();
-        String fName = "/Users/cat/myfile/github/java/text/ht";
+
+        List<String> list1 = new ArrayList<>(Arrays.asList("cat", "dog", "cow"));
+        List<String> list2 = new ArrayList<>(Arrays.asList("cat1", "dog1", "cow1"));
+
+        List<String> list3 = new ArrayList<>(Arrays.asList("do the follows", "different from", "exchange for", "dog"));
+        List<String> list4 = new ArrayList<>(Arrays.asList("withdraw", "negotiation", "withdrawal-none", "dog"));
+
+        List<String> bigList = Aron.mergeListsDuplicate(list3, list4);
+        Aron.printList(bigList);
+        Ut.l();
+        List<String> bigList1 = Aron.mergeListsUnique(list3, list4);
+        Aron.printList(bigList1);
 
         Aron.end();
+    }
+    static void test2(){
+        Aron.beg();
+        // immutable list
+        List<String> list1 = Arrays.asList("cat1", "dog1", "cow1");
+        List<String> list2 = Arrays.asList("cat2", "dog2", "cow2");
+        List<String> list = take(0, list1);
+        Aron.printList(list);
+        Test.t(list.size() == 0);
+
+        List<String> biglist = take(1, list1);
+        Aron.printList(biglist);
+        Test.t(biglist.size() == 1);
+
+        List<String> biglist1 = take(3, list1);
+        Aron.printList(biglist1);
+        Test.t(biglist1.size() == 3);
+
+        List<String> biglist2 = take(4, list1);
+        Aron.printList(biglist2);
+        Test.t(biglist2.size() == 3);
+
+
+        Aron.end();
+    }
+    static void test3(){
+        Aron.beg();
+        // immutable list
+        List<String> list1 = Arrays.asList("cat1", "dog1", "cow1");
+        List<String> list2 = Arrays.asList("cat2", "dog2", "cow2");
+
+        List<String> bigList = drop(0, list1);
+        Test.t(bigList.size() == 3);
+
+        List<String> bigList1 = drop(1, list1);
+        Test.t(bigList1.size() == 2);
+
+        List<String> bigList2 = drop(3, list1);
+        Test.t(bigList2.size() == 0);
+
+        List<String> bigList3 = drop(4, list1);
+        Test.t(bigList3.size() == 0);
+
+        Aron.end();
+    }
+
+    static <T> List<T> take(int n, List<T> list){
+        if(n <= list.size())
+            return list.subList(0, n);
+        else
+            return list;
+
+    }
+    static <T> List<T> drop(int n, List<T> list){
+        if(n <= list.size()){
+            return list.subList(n, list.size());
+        }else{
+            return new ArrayList<T>();
+        }
+    }
+
+    static String getName(String fileName){
+        return FilenameUtils.getName(fileName);
     }
 }
---
+++ cryptoAES/src/main/java/Main.java
@@ -0,0 +1,54 @@
+import javax.crypto.spec.IvParameterSpec;
+import javax.crypto.spec.SecretKeySpec;
+import javax.crypto.Cipher;
+import javax.xml.bind.DatatypeConverter;
+
+// http://stackoverflow.com/questions/15554296/simple-java-aes-encrypt-decrypt-example
+// simple AES encryption decryption in java
+public class Main{
+    public static String encrypt(String key, String initVector, String value) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.ENCRYPT_MODE, skeySpec, iv);
+
+            byte[] encrypted = cipher.doFinal(value.getBytes());
+            System.out.println("encrypted string: " + DatatypeConverter.printBase64Binary(encrypted));
+
+            return DatatypeConverter.printBase64Binary(encrypted);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static String decrypt(String key, String initVector, String encrypted) {
+        try {
+            IvParameterSpec iv = new IvParameterSpec(initVector.getBytes("UTF-8"));
+            SecretKeySpec skeySpec = new SecretKeySpec(key.getBytes("UTF-8"), "AES");
+
+            Cipher cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING");
+            cipher.init(Cipher.DECRYPT_MODE, skeySpec, iv);
+
+            byte[] original = cipher.doFinal(DatatypeConverter.parseBase64Binary(encrypted));
+
+
+            return new String(original);
+        } catch (Exception ex) {
+            ex.printStackTrace();
+        }
+
+        return null;
+    }
+
+    public static void main(String[] args) {
+        String key = "Bar12345Bar12345"; // 128 bit key
+        String initVector = "RandomInitVector"; // 16 bytes IV
+
+        System.out.println(decrypt(key, initVector,
+                encrypt(key, initVector, "Hello World")));
+    }
+}
