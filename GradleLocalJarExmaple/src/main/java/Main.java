5f6a53df1a9f369e2f51e12f4f3e202c3c6025e0
bsdshell <monosurface@gmail.com>
2017-04-02 14:41:08 -0700

    update

---
+++ GradleLocalJarExmaple/src/main/java/Main.java
@@ -0,0 +1,15 @@
+import java.util.*;
+import java.io.*;
+import classfile.*;
+
+public class Main{
+    public static void main(String[] args) {
+        test0();
+    }
+    static void test0(){
+        Aron.beg();
+        Print.pbl("dog");
+        Aron.end();
+    }
+}
+
